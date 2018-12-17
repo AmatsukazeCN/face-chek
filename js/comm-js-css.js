421d1251c71773a60fdd8bc1f04edc2c1e8d8fb1
AmatsukazeCN <875306896@qq.com>
2018-12-17 18:14:52 +0800

    Update comm-js-css.js

--- js/comm-js-css.js
+++ js/comm-js-css.js
@@ -1,5 +1,5 @@
 //在头部引用公共脚本文件和样式表
 document.write('<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">');
-document.write('<script src="http://libs.baidu.com/jquery/1.9.0/jquery.js"></script>');
+document.write('<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>');
 document.write('<script src="https://unpkg.com/vue/dist/vue.js"></script>');
 document.write('<script src="https://unpkg.com/element-ui/lib/index.js"></script>');
